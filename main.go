6d2a0d2485bb743a69d282bb43124a0e9fd7bc62
zorro901 <zoro907@gmail.com>
2023-03-22 00:03:52 +0900

    型変換

--- main.go
+++ main.go
@@ -1,18 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	var x interface{}
-	fmt.Println(x) // <nil>
+	//var i int = 1
+	//fl64 := float64(i)
+	//fmt.Printf("%T\n", i)
+	//fmt.Printf("%T\n", fl64)
+	//
+	//i2 := int(fl64)
+	//fmt.Printf("%T\n", i2)
+	//
+	//fl := 10.5
+	//fmt.Println(int(fl)) // 10 切り捨てになる
 
-	x = 42
-	fmt.Println(x)
+	var s string = "hello"
+	fmt.Printf("%T\n", s)
 
-	x = 3.14
-	fmt.Println(x)
-	x = "hello"
-	fmt.Println(x)
-	x = [3]int{1, 2, 3}
-	fmt.Println(x)
+	//i, _ := strconv.Atoi(s)
+	i, err := strconv.Atoi("s")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(i)
+	fmt.Printf("%T\n", i)
+
+	var i2 int = 200
+	s2 := strconv.Itoa(i2)
+	fmt.Println(s2)
+	fmt.Printf("%T\n", s2)
+
+	var h string = "hello"
+	b := []byte(h)
+	fmt.Println(b)
+
+	h2 := string(b)
+	fmt.Println(h2)
 }
