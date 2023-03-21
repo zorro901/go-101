fab61637cc064c313858439f92ef40cba5e8e88c
zorro901 <zoro907@gmail.com>
2023-03-21 22:54:36 +0900

    Int型

--- main.go
+++ main.go
@@ -2,46 +2,11 @@ package main
 
 import "fmt"
 
-var i5 int = 5
-
-func outer() {
-	var s4 string = "outer"
-	fmt.Println(s4)
-}
-
 func main() {
-	var i int = 10
+	var i int = 100
+	var i2 int64 = 200
 	fmt.Println(i)
-
-	var s string = "Hello Go"
-	fmt.Println(s)
-
-	var t, f bool = true, false
-	fmt.Println(t, f)
-
-	var (
-		i2 int    = 200
-		s2 string = "Golang"
-	)
-	fmt.Println(i2, s2)
-
-	var i3 int
-	var s3 string
-	fmt.Println(i3, s3)
-
-	i3 = 300
-	s3 = "Go"
-	fmt.Println(i3, s3)
-
-	i = 150
-	fmt.Println(i)
-
-	i4 := 450 // 暗黙的な定義 関数内でのみ使用可能
-	i4 = 400
-	fmt.Println(i4)
-
-	fmt.Println(i5)
-
-	outer()
-
+	fmt.Printf("%T\n", i)
+	fmt.Printf("%T\n", int32(i2))
+	fmt.Println(i + int(i2))
 }
