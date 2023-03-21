24ada4e1ed07560513c6be1b9e8e18d6e3eb5ba5
zorro901 <zoro907@gmail.com>
2023-03-21 23:04:27 +0900

    string型

--- main.go
+++ main.go
@@ -3,6 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var t, f bool = true, false
-	fmt.Println(t, f)
+	var s string = "hello golang"
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
+
+	var si string = "300"
+	fmt.Println(si)
+	fmt.Printf("%T\n", si)
+
+	fmt.Println(`test
+	test
+		test`)
+
+	fmt.Println("\"")
+	fmt.Println(`"`)
+
+	fmt.Println(string(s[0]))
 }
