d4e648229e053e58fdc3e243a35a77083c832283
zorro901 <zoro907@gmail.com>
2023-03-21 23:00:47 +0900

    bool型

--- main.go
+++ main.go
@@ -3,22 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	var fl64 float64 = 3.14
-	fmt.Println(fl64)
-
-	fl := 3.14 // 自動だとfloat64型になる
-	fmt.Printf("%T\n", fl)
-
-	var fl32 float32 = 3.14 // あまり使わない
-	fmt.Printf("%T\n", fl32)
-
-	zero := 0.0
-	pinf := 1.0 / zero
-	fmt.Println(pinf)
-
-	ninf := -1.0 / zero
-	fmt.Println(ninf)
-
-	nan := zero / zero
-	fmt.Println(nan)
+	var t, f bool = true, false
+	fmt.Println(t, f)
 }
