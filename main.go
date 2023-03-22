9e012fb105975e228c1761a5dc9c90eb718551ac
zorro901 <zoro907@gmail.com>
2023-03-22 22:30:36 +0900

    算術演算子

--- main.go
+++ main.go
@@ -2,35 +2,14 @@ package main
 
 import "fmt"
 
-const Pi = 3.14
-
-const (
-	URL      = "https://www.google.com"
-	SiteName = "Google"
-)
-
-const (
-	A = 1
-	B
-	C
-	D = "A"
-	E
-	F
-)
-
-const (
-	c0 = iota
-	c1
-	c2
-)
-
-// var Big int = 9223372036854775807 + 1 // overflow
-const Big = 9223372036854775807 + 1
-
 func main() {
-	fmt.Println(Pi)
-	fmt.Println(URL, SiteName)
-	fmt.Println(A, B, C, D, E, F)
-	fmt.Println(Big - 1)
-	fmt.Println(c0, c1, c2)
+	n := 0
+	n += 4
+	n++
+	n--
+	fmt.Println(n)
+
+	s := "hello"
+	s += " world"
+	fmt.Println(s)
 }
