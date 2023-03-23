7b6b29c14385cb76ce2cdc6662d8b880e562390f
zorro901 <zoro907@gmail.com>
2023-03-23 14:37:33 +0900

    無名関数

--- main.go
+++ main.go
@@ -2,35 +2,16 @@ package main
 
 import "fmt"
 
-func Plus(x int, y int) int {
-	return x + y
-}
-
-func Div(x, y int) (int, int) {
-	q := x / y
-	r := x % y
-	return q, r
-}
-
-func Double(price int) (result int) {
-	result = price * 2
-	return
-}
-
-func NoReturn() {
-	fmt.Println("NoReturn")
-	return
-}
-
 func main() {
-	i := Plus(1, 2)
+	f := func(x, y int) int {
+		return x + y
+	}
+	i := f(1, 2)
 	fmt.Println(i)
 
-	i2, i3 := Div(9, 4)
-	fmt.Println(i2, i3)
+	i2 := func(x, y int) int {
+		return x + y
+	}(1, 2)
+	fmt.Println(i2)
 
-	i4 := Double(1000)
-	fmt.Println(i4)
-
-	NoReturn()
 }
