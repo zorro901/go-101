d1156e7fb40aa57f2720bcdd965acbaf7464f641
zorro901 <zoro907@gmail.com>
2023-03-23 14:48:49 +0900

    クロージャー

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
-func CallFunction(f func()) {
-	f()
+import "fmt"
+
+func Later() func(string) string {
+	var store string
+	return func(next string) string {
+		s := store
+		store = next
+		return s
+	}
 }
 
 func main() {
-	CallFunction(func() {
-		println("Hello, world!")
-	})
+	f := Later()
+	fmt.Println(f("hello"))
+	fmt.Println(f("My"))
+	fmt.Println(f("name"))
+	fmt.Println(f("is"))
+	fmt.Println(f("GoLang"))
 }
