0e5007c1ad7cde3bf5e156303acaeffbd6017154
zorro901 <zoro907@gmail.com>
2023-03-23 15:01:42 +0900

    クロージャーを使ったジェネレーター

--- main.go
+++ main.go
@@ -2,20 +2,24 @@ package main
 
 import "fmt"
 
-func integers() func() int {
-	i := 0
-	return func() int {
-		i++
-		return i
-	}
-}
-
 func main() {
-	ints := integers()
-	fmt.Println(ints())
-	fmt.Println(ints())
-	fmt.Println(ints())
+	a := 1
+	if a == 2 {
+		fmt.Println("a is 2")
+	} else if a == 1 {
+		fmt.Println("a is 1")
+	} else {
+		fmt.Println("I dont know")
+	}
 
-	otherints := integers()
-	fmt.Println(otherints())
+	if b := 100; b == 100 {
+		fmt.Println("b is 100")
+	}
+
+	x := 0
+	if x := 2; true {
+		fmt.Println(x) // 2
+	}
+	fmt.Println(x)
+
 }
