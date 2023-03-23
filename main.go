d6a1729855806d5b89b896391a94fbb35d869ea4
zorro901 <zoro907@gmail.com>
2023-03-23 14:39:34 +0900

    関数を返す関数

--- main.go
+++ main.go
@@ -2,16 +2,13 @@ package main
 
 import "fmt"
 
-func main() {
-	f := func(x, y int) int {
-		return x + y
+func ReturnFunc() func() {
+	return func() {
+		fmt.Println("Hello from ReturnFunc()")
 	}
-	i := f(1, 2)
-	fmt.Println(i)
+}
 
-	i2 := func(x, y int) int {
-		return x + y
-	}(1, 2)
-	fmt.Println(i2)
-
+func main() {
+	f := ReturnFunc()
+	f()
 }
