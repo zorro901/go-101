9e553378dbeaaafcbe3b9e742467447cd2cb6380
zorro901 <zoro907@gmail.com>
2023-03-23 19:48:06 +0900

    型switch

--- main.go
+++ main.go
@@ -2,44 +2,58 @@ package main
 
 import "fmt"
 
-func main() {
-	//n := 1
-	//switch n {
-	//case 1, 2:
-	//	println("1 or 2")
-	//case 3, 4:
-	//	println("3 or 4")
-	//default:
-	//	println("default")
-	//}
-
-	//switch n := 2; n {
-	//case 1, 2:
-	//	fmt.Println("1 or 2")
-	//case 3, 4:
-	//	fmt.Println("3 or 4")
-	//default:
-	//	fmt.Println("default")
-	//}
-
-	//n := 6
-	//switch {
-	//case n > 0 && n < 4:
-	//	fmt.Println("n>0 && n<4")
-	//case n > 3 && n < 7:
-	//	fmt.Println("n>3 && n<7")
-	//default:
-	//	fmt.Println("default")
-	//}
-
-	switch n := 2; n {
-	case 1, 2:
-		fmt.Println("1 or 2")
-	case 3, 4:
-		fmt.Println("3 or 4")
-	//case n>3 && n<6:
-	//	fmt.Println("n>3 && n<6")
+func anything(a interface{}) {
+	//fmt.Println(a)
+	switch v := a.(type) {
+	case string:
+		fmt.Println("string", v)
+	case int:
+		fmt.Println("int", v)
+	case bool:
+		fmt.Println("bool", v)
 	default:
-		fmt.Println("default")
+		fmt.Println("unknown", v)
 	}
+}
+
+func main() {
+	anything(42)
+	anything("hello")
+
+	var x interface{} = 3
+	i, isInt := x.(int)
+	fmt.Println(i, isInt)
+	fmt.Println(i + 2)
+
+	f, isFloat64 := x.(float64)
+	fmt.Println(f, isFloat64)
+
+	if x == nil {
+		fmt.Println("x is nil")
+	} else if i, isInt := x.(int); isInt {
+		fmt.Println("x is int", i)
+	} else if s, isString := x.(string); isString {
+		fmt.Println("x is string", s)
+	} else {
+		fmt.Println("I don't know what x is")
+	}
+
+	switch x.(type) {
+	case int:
+		fmt.Println("x is int")
+	case string:
+		fmt.Println("x is string")
+	default:
+		fmt.Println("I don't know what x is")
+	}
+
+	switch v := x.(type) {
+	case int:
+		fmt.Println("x is int", v)
+	case string:
+		fmt.Println("x is string", v)
+	default:
+		fmt.Println("I don't know what x is")
+	}
+
 }
