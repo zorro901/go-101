bf64395e0b63e437d112f8a3099a6a2403343aca
zorro901 <zoro907@gmail.com>
2023-03-23 21:39:23 +0900

    スライス

--- main.go
+++ main.go
@@ -2,14 +2,31 @@ package main
 
 import "fmt"
 
-func init() {
-	fmt.Println("init")
-}
-
-func init() {
-	fmt.Println("init2")
-}
-
 func main() {
-	fmt.Println("Main")
+	var sl []int
+	sl = append(sl, 1)
+	fmt.Println(sl)
+
+	var sl2 []int = []int{1, 2}
+	fmt.Println(sl2)
+
+	sl3 := []string{"a", "b", "c"}
+	fmt.Println(sl3)
+
+	sl4 := make([]int, 5)
+	fmt.Println(sl4)
+
+	sl2[0] = 1000
+	fmt.Println(sl2)
+
+	sl5 := []int{1, 2, 3, 4, 5}
+	fmt.Println(sl5)
+
+	fmt.Println(sl5[0])
+	fmt.Println(sl5[1:3])
+	fmt.Println(sl5[:2])
+	fmt.Println(sl5[2:])
+	fmt.Println(sl5[:])
+	fmt.Println(sl5[len(sl5)-1])
+	fmt.Println(sl5[1 : len(sl5)-1])
 }
