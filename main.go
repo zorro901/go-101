d677f79b8340d847ea93df078459ae3496e8f76e
zorro901 <zoro907@gmail.com>
2023-03-23 15:05:10 +0900

    エラーハンドリング

--- main.go
+++ main.go
@@ -1,25 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	a := 1
-	if a == 2 {
-		fmt.Println("a is 2")
-	} else if a == 1 {
-		fmt.Println("a is 1")
-	} else {
-		fmt.Println("I dont know")
+	var s string = "A"
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Println(err)
 	}
-
-	if b := 100; b == 100 {
-		fmt.Println("b is 100")
-	}
-
-	x := 0
-	if x := 2; true {
-		fmt.Println(x) // 2
-	}
-	fmt.Println(x)
-
+	fmt.Println(i)
 }
