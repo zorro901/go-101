72944c1a8a6c8685c75c1c0d490c3e7556964b26
zorro901 <zoro907@gmail.com>
2023-03-27 08:58:10 +0900

    スライス 可変長引数

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import "fmt"
 
-func main() {
-	sl := []string{"A", "B", "C"}
-	fmt.Println(sl)
-
-	//for i, v := range sl {
-	//	fmt.Println(i, v)
-	//}
-
-	for i := 0; i < len(sl); i++ {
-		fmt.Println(i, sl[i])
+func Sum(s ...int) int {
+	n := 0
+	for _, v := range s {
+		n += v
 	}
+	return n
+}
+
+func main() {
+	fmt.Println(Sum(1, 2, 3))
+
+	sl := []int{1, 2, 3}
+	fmt.Println(Sum(sl...))
 }
