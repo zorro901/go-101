fb15c66a4d6dcef030e65e9bc02bc29bb30faabe
zorro901 <zoro907@gmail.com>
2023-03-27 08:55:00 +0900

    スライス for

--- main.go
+++ main.go
@@ -3,20 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	//sl := []int{100, 200}
-	//sl2 := sl
-	//sl2[0] = 1000
-	//fmt.Println(sl) // 同じアドレスを指す、参照渡し
-	//
-	//var i int = 10
-	//i2 := i
-	//i2 = 100
-	//fmt.Println(i, i2) // 基本型は参照渡しにならない
+	sl := []string{"A", "B", "C"}
+	fmt.Println(sl)
 
-	sl := []int{1, 2, 3, 4, 5}
-	sl2 := make([]int, 5, 10)
-	fmt.Println(sl2)
-	n := copy(sl2, sl)
-	fmt.Println(n, sl2)
+	//for i, v := range sl {
+	//	fmt.Println(i, v)
+	//}
 
+	for i := 0; i < len(sl); i++ {
+		fmt.Println(i, sl[i])
+	}
 }
