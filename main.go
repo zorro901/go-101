aabb475acd7d27821564898a5d8eba94a0de1d46
zorro901 <zoro907@gmail.com>
2023-03-28 04:31:45 +0900

    struct メソッド

--- main.go
+++ main.go
@@ -5,53 +5,27 @@ import "fmt"
 type User struct {
 	Name string
 	Age  int
-	//X, Y int
 }
 
-func UpdateUser(user User) {
-	user.Name = "A"
-	user.Age = 1000
+func (u User) SayName() {
+	fmt.Println(u.Name)
 }
 
-func UpdateUser2(user *User) {
-	user.Name = "A"
-	user.Age = 1000
+func (u User) SetName(name string) {
+	u.Name = name
+}
+
+// メソッドのレシーバーはポインタ型が望ましい
+func (u *User) SetName2(name string) {
+	u.Name = name
 }
 
 func main() {
-	var user1 User
-	user1.Name = "user1"
-	user1.Age = 10
-	fmt.Println(user1)
+	user1 := User{Name: "John", Age: 20}
+	user1.SetName2("Jane")
+	user1.SayName()
 
-	user2 := User{}
-	user2.Name = "Jane"
-	fmt.Println(user2)
-
-	user3 := User{
-		Name: "Bob",
-	}
-	fmt.Println(user3)
-
-	user4 := User{
-		"user4", 40,
-	}
-	fmt.Println(user4)
-
-	user5 := User{
-		Name: "Bob",
-	}
-	fmt.Println(user5)
-
-	user6 := new(User) // ポインタ型で宣言できる
-	fmt.Println(user6)
-
-	user7 := &User{} // ポインタ型でこちらの方が書かれやすい
-	fmt.Println(user7)
-
-	UpdateUser(user1)
-	UpdateUser2(user7)
-
-	fmt.Println(user1)
-	fmt.Println(user7)
+	user2 := &User{Name: "Jane"}
+	user2.SetName2("John")
+	user2.SayName()
 }
