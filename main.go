512770920d84ab078f11119040f8235bfd52e319
zorro901 <zoro907@gmail.com>
2023-03-27 09:05:18 +0900

    マップ

--- main.go
+++ main.go
@@ -2,17 +2,43 @@ package main
 
 import "fmt"
 
-func Sum(s ...int) int {
-	n := 0
-	for _, v := range s {
-		n += v
-	}
-	return n
-}
-
 func main() {
-	fmt.Println(Sum(1, 2, 3))
+	var m = map[string]int{
+		"A": 100,
+		"B": 200,
+	}
+	fmt.Println(m)
 
-	sl := []int{1, 2, 3}
-	fmt.Println(Sum(sl...))
+	m2 := map[string]int{
+		"A": 100,
+		"B": 200,
+	}
+	fmt.Println(m2)
+
+	m3 := map[int]string{
+		1: "A",
+		2: "B",
+	}
+	fmt.Println(m3)
+
+	m4 := map[int]string{}
+	m4[1] = "JAPAN"
+	m4[2] = "USA"
+	fmt.Println(m4)
+
+	fmt.Println(m["A"])
+	fmt.Println(m4[2])
+	fmt.Println(m4[3])
+
+	_, ok := m4[3]
+	if !ok {
+		fmt.Println("Not found")
+	}
+
+	m4[3] = "CHINA"
+	fmt.Println(m4)
+	delete(m4, 3)
+	fmt.Println(m4)
+
+	fmt.Println(len(m4))
 }
