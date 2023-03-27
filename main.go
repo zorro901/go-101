9edf6f82237449ad7bac5ae0bacbbdec0b2c2456
zorro901 <zoro907@gmail.com>
2023-03-28 05:03:26 +0900

    interface カスタムエラー

--- main.go
+++ main.go
@@ -2,35 +2,26 @@ package main
 
 import "fmt"
 
-type Stringify interface {
-	ToString() string
+type MyError struct {
+	Message string
+	ErrCode int
 }
 
-type Person struct {
-	Name string
-	Age  int
+func (e *MyError) Error() string {
+	return e.Message
 }
 
-func (p *Person) ToString() string {
-	return fmt.Sprintf("Name=%v, Age=%v", p.Name, p.Age)
-}
-
-type Car struct {
-	Number string
-	Model  string
-}
-
-func (p *Car) ToString() string {
-	return fmt.Sprintf("Number=%v, Model=%v", p.Number, p.Model)
+func RaiseError() error {
+	return &MyError{Message: "カスタムエラーが発生しました", ErrCode: 1234}
 }
 
 func main() {
-	vs := []Stringify{
-		&Person{Name: "Tom", Age: 18},
-		&Car{Number: "123", Model: "BMW"},
+	err := RaiseError()
+	fmt.Println(err.Error())
+
+	e, ok := err.(*MyError)
+	if ok {
+		fmt.Println(e.ErrCode)
 	}
 
-	for _, v := range vs {
-		fmt.Println(v.ToString())
-	}
 }
