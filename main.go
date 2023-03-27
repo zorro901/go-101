4dceb1e50feda77350e3b6e9e495b84ce4edd74f
zorro901 <zoro907@gmail.com>
2023-03-28 02:58:55 +0900

    チャネル close

--- main.go
+++ main.go
@@ -5,25 +5,37 @@ import (
 	"time"
 )
 
-func receiver(c chan int) {
+func receiver(name string, ch chan int) {
 	for {
-		i := <-c
-		fmt.Println(i)
+		i, ok := <-ch
+		if !ok {
+			break
+		}
+		fmt.Println(name, i)
 	}
+	fmt.Println(name + " END")
 }
 
 func main() {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
+	ch1 := make(chan int, 2)
 
-	go receiver(ch1)
-	go receiver(ch2)
+	//ch1 <- 1 // close後、送信は出来ないが受信はできる
+	//close(ch1)
+	////fmt.Println(<-ch1)
+	//
+	//i, ok := <-ch1
+	//fmt.Println(i, ok) // closeしてデータがない場合はfalse
+
+	go receiver("1.goroutin", ch1)
+	go receiver("2.goroutin", ch1)
+	go receiver("3.goroutin", ch1)
 
 	i := 0
 	for i < 100 {
 		ch1 <- i
-		ch2 <- i
-		time.Sleep(50 * time.Millisecond)
 		i++
 	}
+	close(ch1)
+	time.Sleep(3 * time.Second)
+
 }
