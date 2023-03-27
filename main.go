f7ed666d36f6e638f213cd98d9803159812e017c
zorro901 <zoro907@gmail.com>
2023-03-28 04:58:15 +0900

    interface 異なる型に共通の性質を付与する

--- main.go
+++ main.go
@@ -2,19 +2,35 @@ package main
 
 import "fmt"
 
-type MyInt int
+type Stringify interface {
+	ToString() string
+}
 
-func (mi MyInt) Print() {
-	fmt.Println(mi)
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p *Person) ToString() string {
+	return fmt.Sprintf("Name=%v, Age=%v", p.Name, p.Age)
+}
+
+type Car struct {
+	Number string
+	Model  string
+}
+
+func (p *Car) ToString() string {
+	return fmt.Sprintf("Number=%v, Model=%v", p.Number, p.Model)
 }
 
 func main() {
-	var mi MyInt
-	fmt.Println(mi)
-	fmt.Printf("%T\n", mi)
+	vs := []Stringify{
+		&Person{Name: "Tom", Age: 18},
+		&Car{Number: "123", Model: "BMW"},
+	}
 
-	//i := 100
-	//fmt.Println(mi + i) // 独自型と計算は出来ない
-
-	mi.Print()
+	for _, v := range vs {
+		fmt.Println(v.ToString())
+	}
 }
