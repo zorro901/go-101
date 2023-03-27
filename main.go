31e0ae1b3d3f9d6e6420afc6d2feb523146b78a3
zorro901 <zoro907@gmail.com>
2023-03-28 04:37:26 +0900

    struct 埋め込み

--- main.go
+++ main.go
@@ -2,30 +2,27 @@ package main
 
 import "fmt"
 
+type T struct {
+	User
+}
+
 type User struct {
 	Name string
 	Age  int
 }
 
-func (u User) SayName() {
-	fmt.Println(u.Name)
-}
-
-func (u User) SetName(name string) {
-	u.Name = name
-}
-
-// メソッドのレシーバーはポインタ型が望ましい
-func (u *User) SetName2(name string) {
-	u.Name = name
+func (u *User) SetName() {
+	u.Name = "James Bond"
 }
 
 func main() {
-	user1 := User{Name: "John", Age: 20}
-	user1.SetName2("Jane")
-	user1.SayName()
-
-	user2 := &User{Name: "Jane"}
-	user2.SetName2("John")
-	user2.SayName()
+	t := T{
+		User: User{
+			Name: "John",
+			Age:  20,
+		},
+	}
+	fmt.Println(t.Name) // T構造体で省略されている場合は直接参照できる
+	t.SetName()
+	fmt.Println(t.Name)
 }
