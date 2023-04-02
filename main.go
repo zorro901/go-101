60dec32eb8a1a7dba807cc5c04affe97f33f2216
zorro901 <zoro907@gmail.com>
2023-04-02 23:05:18 +0900

    net/http server

--- main.go
+++ main.go
@@ -2,64 +2,31 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"html/template"
 	"log"
 	"net/http"
-	"net/url"
 )
 
-func main() {
-	//u, _ := url.Parse("http://example.com/search?a=1&b=2#top")
-	//fmt.Println(u.Scheme)
-	//fmt.Println(u.Host)
-	//fmt.Println(u.Path)
-	//fmt.Println(u.RawQuery)
-	//fmt.Println(u.Fragment)
-	//
-	//fmt.Println(u.IsAbs()) // 絶対URLならtrue
-	//fmt.Println(u.Query()) // クエリをマップで取得する
-	//
-	//url := &url.URL{}
-	//url.Scheme = "https:"
-	//url.Host = "google.com"
-	//q := url.Query()
-	//q.Set("q", "Golang") // クエリをセット
-	//url.RawQuery = q.Encode()
-	//
-	//fmt.Println(url)
+type MyHandler struct{}
 
-	//GETメソッド
-	res, _ := http.Get("https://example.com")
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
 
-	fmt.Println(res.StatusCode)
-
-	fmt.Println(res.Proto)
-
-	fmt.Println(res.Header["Date"])
-	fmt.Println(res.Header["Content-Type"])
-
-	fmt.Println(res.Request.Method)
-	fmt.Println(res.Request.URL)
-
-	defer res.Body.Close() // リソースを解放
-	body, _ := ioutil.ReadAll(res.Body)
-	fmt.Print(string(body))
-
-	//------------------------------------
-	//POSTメソッド
-
-	vs := url.Values{}
-
-	vs.Add("id", "1")
-	vs.Add("message", "メッセージ")
-	fmt.Println(vs.Encode()) // => "id=1&message=%E3%83%A1%E3%83%83%E3%82%BB%E3%83@<dtp>{lb}%BC%E3%82%B8"
-
-	res, err := http.PostForm("https://example.com/", vs)
+func top(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("tmpl.html")
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
-	defer res.Body.Close()
-	body, _ = ioutil.ReadAll(res.Body)
-	fmt.Print(string(body))
+	err = t.Execute(w, "Hello World111!")
+	if err != nil {
+		return
+	}
+}
 
+func main() {
+	http.HandleFunc("/top", top)
+
+	//http.ListenAndServe(":8080", &MyHandler{})
+	http.ListenAndServe(":8080", nil)
 }
