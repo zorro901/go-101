957983438c24d15e16822efcbaf412f59a13df1f
zorro901 <zoro907@gmail.com>
2023-04-02 18:47:38 +0900

    context

--- main.go
+++ main.go
@@ -1,91 +1,52 @@
 package main
 
+/*
+type Context interface {
+    Deadline() (deadline time.Time, ok bool)
+    Done() <-chan struct{}
+    Err() error
+    Value(key interface{}) interface{}
+}
+*/
+
 import (
+	"context"
 	"fmt"
-	"sort"
+	"time"
 )
 
-type Entry struct {
-	Name  string
-	Value int
+func longProcess(ctx context.Context, ch chan string) {
+	fmt.Println("開始")
+	time.Sleep(2 * time.Second)
+	fmt.Println("終了")
+	ch <- "実行結果"
 }
 
-type List []Entry
-
-func (l List) Len() int {
-	return len(l)
-}
-func (l List) Swap(i, j int) {
-	l[i], l[j] = l[j], l[i]
-}
-
-func (l List) Less(i, j int) bool {
-	if l[i].Value == l[j].Value {
-		return l[i].Name < l[j].Name
-	} else {
-		return l[i].Value < l[j].Value
-	}
-}
 func main() {
+	ch := make(chan string)                                // チャネルを作成
+	ctx := context.Background()                            // コンテキストを作成
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second) // 1秒間のタイムアウトを設定
 
-	i := []int{5, 3, 2, 4, 5, 6, 4, 8, 9, 8, 7, 10}
-	s := []string{"a", "z", "j"}
+	defer cancel()
 
-	fmt.Println(i, s)
+	go longProcess(ctx, ch) // 処理に２秒かかる
 
-	sort.Ints(i)    // [5 3 2 4 5 6 4 8 9 8 7 10] [a z j]
-	sort.Strings(s) // [a j z]
+	//cancel()
 
-	fmt.Println(i, s)
+L:
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("##########Error###########")
+			fmt.Println(ctx.Err())
+			break L
+		case s := <-ch:
+			fmt.Println(s)
+			fmt.Println("success")
+			break L
+		}
 
-	el := []Entry{
-		{"A", 20},
-		{"F", 40},
-		{"i", 30},
-		{"b", 10},
-		{"t", 15},
-		{"y", 30},
-		{"c", 30},
-		{"w", 30},
 	}
 
-	fmt.Println(el)
-
-	// 名前を昇順にソート
-	sort.Slice(el, func(i, j int) bool { return el[i].Name < el[j].Name })
-	fmt.Println("名前を昇順にソート")
-	fmt.Println(el)
-	// 値順にソート、名前順は上書きされる
-	sort.Slice(el, func(i, j int) bool { return el[i].Value < el[j].Value })
-
-	fmt.Println("---------")
-	fmt.Println(el)
-	fmt.Println("---------")
-
-	// 名前順を維持して値順にソート
-	sort.SliceStable(el, func(i, j int) bool { return el[i].Name < el[j].Name })
-	sort.SliceStable(el, func(i, j int) bool { return el[i].Value < el[j].Value })
-
-	fmt.Println("---------")
-	fmt.Println(el)
-	fmt.Println("---------")
-
-	m := map[string]int{"ada": 1, "hoge": 4, "basha": 3, "poeni": 3}
-
-	lt := List{}
-	for k, v := range m {
-		e := Entry{k, v}
-		lt = append(lt, e)
-	}
-
-	//sort.SliceStable(lt, func(i, j int) bool { return lt[i].Name < lt[j].Name })
-	//fmt.Println(lt)
-
-	sort.Sort(lt)
-
-	fmt.Println(lt)
-
-	sort.Sort(sort.Reverse(lt))
-	fmt.Println(lt)
-
+	fmt.Println("ループ抜けた")
 }
