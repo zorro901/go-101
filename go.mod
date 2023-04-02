fd4064c0b7d55ae476c36607b2c3f88db2b1e972
zorro901 <zoro907@gmail.com>
2023-04-03 01:08:23 +0900

    database / sqlite3

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module go-101
 
 go 1.20
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/lib/pq v1.10.7
+	github.com/mattn/go-sqlite3 v1.14.16
+	gopkg.in/go-ini/ini.v1 v1.67.0
+)
+
+require github.com/stretchr/testify v1.8.2 // indirect
--- main.go
+++ main.go
@@ -1,32 +1,57 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
-	"net/http"
 )
 
-type MyHandler struct{}
+var DbConnection *sql.DB
 
-func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
-}
-
-func top(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("tmpl.html")
-	if err != nil {
-		log.Println(err)
-	}
-	err = t.Execute(w, "Hello World111!")
-	if err != nil {
-		return
-	}
+type Person struct {
+	Name string
+	Age  int
 }
 
 func main() {
-	http.HandleFunc("/top", top)
+	DbConnection, _ := sql.Open("sqlite3", "./example.sql")
+	defer DbConnection.Close()
+	//cmd := `CREATE TABLE IF NOT EXISTS persons(
+	//            name STRING,
+	//			age  INT)`
+	//_, err := DbConnection.Exec(cmd)
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
 
-	//http.ListenAndServe(":8080", &MyHandler{})
-	http.ListenAndServe(":8080", nil)
+	//cmd := "INSERT INTO persons (name, age) VALUES (?, ?)"
+	//_, err := DbConnection.Exec(cmd, "Nancy", 20)
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+
+	//cmd := "UPDATE persons SET age = ? WHERE name = ?"
+	//_, err := DbConnection.Exec(cmd, 25, "Nancy")
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+
+	//cmd := "SELECT * FROM persons where age = ?"
+	//row := DbConnection.QueryRow(cmd, 25)
+	//var p Person
+	//err := row.Scan(&p.Name, &p.Age)
+	//if err != nil {
+	//	if err == sql.ErrNoRows {
+	//		log.Println("No row")
+	//	} else {
+	//		log.Println(err)
+	//	}
+	//}
+	//fmt.Println(p.Name, p.Age)
+
+	cmd := "DELETE FROM persons WHERE name = ?"
+	_, err := DbConnection.Exec(cmd, "Nancy")
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
