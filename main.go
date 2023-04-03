ab47a9bd97005fc6042628e886f7f84a1cb1838b
zorro901 <zoro907@gmail.com>
2023-04-04 08:50:07 +0900

    generics set

--- main.go
+++ main.go
@@ -2,9 +2,26 @@ package main
 
 import "fmt"
 
+type Set[T comparable] map[T]struct{}
+
+func NewSet[T comparable](xs ...T) Set[T] {
+	s := make(Set[T])
+	for _, v := range xs {
+		s.Add(v)
+	}
+	return s
+}
+
+func (s Set[T]) Add(x T) {
+	s[x] = struct{}{}
+}
+
+func (s Set[T]) Remove(x T) {
+	delete(s, x)
+}
+
 func main() {
-	var a any = 1
-	a = "a"
-	a = true
-	fmt.Println(a)
+	s := NewSet(1, 2, 3)
+	s.Remove(3)
+	fmt.Println(s)
 }
