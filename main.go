a4cdeaebae16dc6bcf7cb2ea9bc43656651e2940
zorro901 <zoro907@gmail.com>
2023-04-04 08:44:07 +0900

    generics any

--- main.go
+++ main.go
@@ -2,17 +2,9 @@ package main
 
 import "fmt"
 
-type T[A any, B []C, C *A] struct {
-	a A
-	b B
-	c C
-}
-
-func (t T[A, B, C]) m(x int) {
-
-}
-
 func main() {
-	var t T[int, []*int, *int]
-	fmt.Printf("A: %T, B: %T, C: %T\n", t.a, t.b, t.c)
+	var a any = 1
+	a = "a"
+	a = true
+	fmt.Println(a)
 }
