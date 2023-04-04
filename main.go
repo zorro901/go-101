2d153ce2a594272aafa3720054c064092e5cb6b9
zorro901 <zoro907@gmail.com>
2023-04-04 09:43:48 +0900

    Goroutine deadlock

--- main.go
+++ main.go
@@ -3,29 +3,41 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
+
+type value struct {
+	mu    sync.Mutex
+	value int
+	name  string
+}
 
 func main() {
 	var wg sync.WaitGroup
-	var memoryAccess sync.Mutex
-	var data int // クリティカルセクション
 
-	wg.Add(1)
-	go func() {
+	printSum := func(v1, v2 *value) {
 		defer wg.Done()
-		memoryAccess.Lock() // Lockのコストは小さくない
-		data++
-		memoryAccess.Unlock()
-	}()
+		v1.mu.Lock()
+		fmt.Printf("%v がロックを取得しました\n", v1.name)
+		defer v1.mu.Unlock()
+
+		time.Sleep(2 * time.Second)
+
+		v2.mu.Lock()
+		fmt.Printf("%v がロックを取得しました\n", v2.name)
+		defer v2.mu.Unlock()
+
+		fmt.Println(v1.value + v2.value)
+	}
+
+	var a value = value{name: "a"}
+	var b value = value{name: "b"}
+
+	wg.Add(2)
+
+	go printSum(&a, &b)
+	go printSum(&b, &a)
 
 	wg.Wait()
-
-	memoryAccess.Lock()
-	if data == 0 {
-		fmt.Println(0)
-	} else {
-		fmt.Println(data)
-	}
-	memoryAccess.Unlock()
 
 }
