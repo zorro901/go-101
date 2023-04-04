05575a98e75901e9e08a2d915406771df869c600
zorro901 <zoro907@gmail.com>
2023-04-04 09:20:32 +0900

    Goroutineのメモリ消費量

--- main.go
+++ main.go
@@ -2,33 +2,44 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 )
 
+func memConsumed() uint64 {
+	runtime.GC() // 使われていないメモリを解放する
+
+	var s runtime.MemStats
+
+	runtime.ReadMemStats(&s) // メモリの状態を取得
+
+	return s.Sys
+}
+
 func main() {
+	var ch <-chan interface{}
 	var wg sync.WaitGroup
 
-	//say := "Hello"
-	//
-	//wg.Add(1)
-	//
-	//go func() {
-	//	defer wg.Done()
-	//	say = "Good bye"
-	//}()
-	//
-	//wg.Wait()
-	//
-	//fmt.Println(say) // クロージャーの編集が行われる
-
-	tasks := []string{"A", "B", "C"}
-	for _, task := range tasks {
-		wg.Add(1)
-		go func(task string) {
-			defer wg.Done()
-			fmt.Println(task)
-		}(task)
+	noop := func() {
+		wg.Done()
+		<-ch // チャネルの受信を永遠に待つ
 	}
+
+	const numGoroutines = 1000000
+
+	wg.Add(numGoroutines)
+
+	before := memConsumed()
+
+	for i := 0; i < numGoroutines; i++ {
+		go noop()
+	}
+
 	wg.Wait()
+
+	after := memConsumed()
+
+	// 8.878kb
+	fmt.Printf("%.3fkb", float64(after-before)/numGoroutines/1000) // 小数点3桁まで表示
 
 }
