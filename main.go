af64589fdfc432563801ded9aa647f94d264f89f
zorro901 <zoro907@gmail.com>
2023-04-04 09:35:52 +0900

    Goroutine 競合

--- main.go
+++ main.go
@@ -2,42 +2,30 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 	"sync"
 )
 
-func Hello(wg *sync.WaitGroup, id int) {
-	defer wg.Done()
-	fmt.Printf("Hello Goroutine %d\n", id)
-}
-
 func main() {
 	var wg sync.WaitGroup
+	var memoryAccess sync.Mutex
+	var data int // クリティカルセクション
 
-	//wg.Add(1)
-	//go func() {
-	//	defer wg.Done()
-	//	fmt.Println("1st Goroutine Start")
-	//	time.Sleep(1 * time.Second)
-	//	fmt.Println("1st Goroutine Done")
-	//}()
-	//
-	//wg.Add(1)
-	//go func() {
-	//	defer wg.Done()
-	//	fmt.Println("2st Goroutine Start")
-	//	time.Sleep(1 * time.Second)
-	//	fmt.Println("2st Goroutine Done")
-	//}()
-	//
-	//wg.Wait()
-
-	var CPU int = runtime.NumCPU()
-
-	wg.Add(CPU)
-	for i := 0; i < CPU; i++ {
-		go Hello(&wg, i)
-	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		memoryAccess.Lock() // Lockのコストは小さくない
+		data++
+		memoryAccess.Unlock()
+	}()
 
 	wg.Wait()
+
+	memoryAccess.Lock()
+	if data == 0 {
+		fmt.Println(0)
+	} else {
+		fmt.Println(data)
+	}
+	memoryAccess.Unlock()
+
 }
