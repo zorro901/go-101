03dba49898dbb87143a4fa78aca11b90e5970b65
zorro901 <zoro907@gmail.com>
2023-04-20 22:28:34 +0900

    Database

---
+++ migrate/migrate.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"go-101/db"
+	"go-101/model"
+)
+
+func main() {
+	dbConn := db.NewDB()
+	defer fmt.Println("Successfully Migrated")
+	defer db.CloseDB(dbConn)
+	dbConn.AutoMigrate(&model.User{}, &model.Task{})
+}
