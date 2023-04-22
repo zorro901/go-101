9ac629f4aa56c91343294c16fd44a00a54034584
zorro901 <zoro907@gmail.com>
2023-04-22 10:39:42 +0900

    Auth component

--- front/react-todo/src/components/Todo.tsx
+++ front/react-todo/src/components/Todo.tsx
@@ -1,8 +1,18 @@
-const Todo = () => {
-  return (
-    <>
+import { useMutateAuth } from '../hooks/useMutateAuth';
+import { ShieldCheckIcon } from '@heroicons/react/20/solid';
 
-    </>
+const Todo = () => {
+  const { logoutMutation } = useMutateAuth()
+  const logout = async () => {
+    await logoutMutation.mutateAsync()
+  }
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono">
+      <div className="flex items-center my-3">
+        <ShieldCheckIcon className="h-8 w-8 mr-3 text-indigo-500 cursor-pointer"
+        onClick={logout}/>
+      </div>
+    </div>
   )
 }
 
